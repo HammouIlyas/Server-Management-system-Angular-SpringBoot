1cb9d83d243a764c27e89380535e470e158becfb
ilyas hammou <ilyashammou.prof@gmail.com>
2022-09-25 00:33:20 +0100

    adding initial values for the database

--- BackEnd/src/main/java/com/example/servermanagementsystem_backend/ServerManagementSystemBackendApplication.java
+++ BackEnd/src/main/java/com/example/servermanagementsystem_backend/ServerManagementSystemBackendApplication.java
@@ -1,7 +1,17 @@
 package com.example.servermanagementsystem_backend;
 
+import com.example.servermanagementsystem_backend.Repositories.ServerRepo;
+import com.example.servermanagementsystem_backend.model.Server;
+import com.example.servermanagementsystem_backend.model.ServerStatus;
+import com.example.servermanagementsystem_backend.service.implementation.ServerServicesIplementation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static com.example.servermanagementsystem_backend.model.ServerStatus.SERVER_DOWN;
+import static com.example.servermanagementsystem_backend.model.ServerStatus.SERVER_UP;
 
 @SpringBootApplication
 public class ServerManagementSystemBackendApplication {
@@ -9,5 +19,19 @@ public class ServerManagementSystemBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerManagementSystemBackendApplication.class, args);
     }
-
+    @Autowired
+    private ServerRepo serverRepo;
+    @Bean
+    CommandLineRunner run(){
+        return args -> {
+            serverRepo.save(new Server(null,"192.168.1.156","Fedora Linux","16GB",
+                    "personnal laptop","http://....", SERVER_UP));
+            serverRepo.save(new Server(null,"192.168.1.146","Ubuntu Linux","32GB",
+                    "Orange server","http://....", SERVER_UP));
+            serverRepo.save(new Server(null,"192.168.1.136","Fedora Linux","64GB",
+                    "Mail server","http://....", SERVER_DOWN));
+            serverRepo.save(new Server(null,"192.168.1.126","Debian Linux","128GB",
+                    "Web server","http://....", SERVER_UP));
+        };
+    }
 }
--- BackEnd/src/main/java/com/example/servermanagementsystem_backend/model/Server.java
+++ BackEnd/src/main/java/com/example/servermanagementsystem_backend/model/Server.java
@@ -28,4 +28,6 @@ public class Server {
     private String memory;
     private String imageUrl;
     private ServerStatus status;
+
+
 }
