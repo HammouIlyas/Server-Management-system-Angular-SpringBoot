75834c78cc703218b8a15133dd267e4fbb1fc706
ilyas hammou <ilyashammou.prof@gmail.com>
2022-09-25 00:04:26 +0100

    adding server's end point save

--- BackEnd/src/main/java/com/example/servermanagementsystem_backend/controllers/ServerController.java
+++ BackEnd/src/main/java/com/example/servermanagementsystem_backend/controllers/ServerController.java
@@ -7,11 +7,9 @@ import com.example.servermanagementsystem_backend.service.implementation.ServerS
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Map;
@@ -49,6 +47,19 @@ public class ServerController {
                         .message(server.getStatus() == SERVER_UP ? "Ping success" : "Ping failed")
                         .status(HttpStatus.OK)
                         .statusCode(HttpStatus.OK.value())
+                        .build())
+                ;
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<HttpResponse> saveServer(@RequestBody @Valid Server server) throws IOException {
+        return ResponseEntity.ok(
+                HttpResponse.builder()
+                        .timeStamp(LocalDate.now())
+                        .data(Map.of("server",servicesIplementation.createServer(server)))
+                        .message("server created successfully")
+                        .status(HttpStatus.CREATED)
+                        .statusCode(HttpStatus.CREATED.value())
                         .build())
                 ;
     }
